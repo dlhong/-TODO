e9acc71a6c24a7a6e551ce95539126c74d972264
Alexandros Zoupas <alexandros.p.zoupas@gmail.com>
2022-08-20 14:56:37 +0300

    new create activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,29 @@
 class ActivitiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
+
   def index
     @activities = Activity.all
   end
+
+  def new
+    @activity = Activity.new
+    #authorize @posting
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user
+
+    if @activity.save
+      redirect_to activity_path(@activity)
+    else
+      render :new
+    end
+    #authorize @activity
+  end
+
+  def activity_params
+    params.require(:activity).permit(:summary, :name, :address, :contact_info, :price)
+  end
+
 end
