fcb983b2cc3f3ec336388a4897341ba7f655561f
yasin genc <ysngenc61@gmail.com>
2022-08-23 20:26:41 +0200

    A user can log out now

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: %i[index]
   before_action :set_activity, only: %i[show]
 
   def index
