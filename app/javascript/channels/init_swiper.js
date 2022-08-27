03363c15bc753603ff8a15b7bb02e6fd93472fd3
daniel hong <dlhong197@gmail.com>
2022-08-27 12:53:16 +0200

    finished swipe js

--- app/javascript/channels/init_swiper.js
+++ app/javascript/channels/init_swiper.js
@@ -1,26 +1,56 @@
   // import Swiper and modules styles
-  import Swiper, { Navigation, Pagination } from 'swiper';
+  // import Swiper, { Navigation, Pagination } from 'swiper';
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 50,
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 40
-      }
-    }
+    // breakpoints:{
+    //     0: {
+    //         slidesPerView: 1,
+    //     },
+    //     520: {
+    //         slidesPerView: 2,
+    //     },
+    //     950: {
+    //         slidesPerView: 4,
+    //     },
+    // },
   });
+
+
+    // const swiper = new Swiper('.swiper', {
+    //   slidesPerView: 3,
+    //   spaceBetween: 50,
+    //   // Responsive breakpoints
+    //   breakpoints: {
+    //     // when window width is >= 320px
+    //     320: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 20
+    //     },
+    //     // when window width is >= 480px
+    //     480: {
+    //       slidesPerView: 3,
+    //       spaceBetween: 30
+    //     },
+    //     // when window width is >= 640px
+    //     640: {
+    //       slidesPerView: 4,
+    //       spaceBetween: 40
+    //     }
+    //   }
+    // });
   export {swiper};
