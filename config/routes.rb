975af7375ab5c66d9b45f3b4c10ddfac30db5278
yasin genc <ysngenc61@gmail.com>
2022-08-27 11:33:54 +0200

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :activities, only: %I[index new create show] do
-    resources :savings, only: %i[new create destroy edit update]
+    resources :savings
     resources :reviews, only: %i[new create destroy]
   end
 end
