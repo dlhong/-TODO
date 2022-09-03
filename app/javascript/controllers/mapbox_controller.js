5da399484e017d750c7de3fd52dd7d4715be34a9
Alexandros Zoupas <alexandros.p.zoupas@gmail.com>
2022-09-03 16:10:58 +0300

    map2

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -21,7 +21,10 @@ export default class extends Controller {
   _addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window) // add this
-      new mapboxgl.Marker()
+      new mapboxgl.Marker({
+        color: "#FAC494",
+        draggable: true
+        })
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // add this
         .addTo(this.map)
