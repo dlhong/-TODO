00cce01914e9473a89ef9051bca26463c065acd5
Alexandros Zoupas <alexandros.p.zoupas@gmail.com>
2022-09-06 21:29:39 +0300

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,18 +17,36 @@ require "open-uri"
 #               price: rand(50..200))
 # end
 
-url = "https://api.openbrewerydb.org/breweries"
+# url = "https://api.openbrewerydb.org/breweries"
+# activity_serialized = URI.open(url).read
+# activity = JSON.parse(activity_serialized)
+
+# puts "Starting the seed"
+# i = 1
+# for i in 1..15 do
+#   Activity.create(summary: activity[i]["brewery_type"],
+#                   name: activity[i]["name"],
+#                   address: [activity[i]["street"], activity[i]["city"]],
+#                   contact_info: activity[i]["phone"],
+#                   price: rand(0..100))
+#   i += 1
+#   puts "done #{i}object"
+# end
+
+
+url = "https://api.seatgeek.com/2/events?client_id=Mjg5MTM0MTJ8MTY2MjQ4MjA0NS43Mzc3NjQx"
 activity_serialized = URI.open(url).read
 activity = JSON.parse(activity_serialized)
 
 puts "Starting the seed"
 i = 1
-for i in 1..15 do
-  Activity.create(summary: activity[i]["brewery_type"],
-                  name: activity[i]["name"],
-                  address: [activity[i]["street"], activity[i]["city"]],
-                  contact_info: activity[i]["phone"],
+for i in 1..6 do
+  Activity.create(summary: activity["events"][i]["type"],
+                  name: activity["events"][i]["name"],
+                  address: activity["events"][i]["address"],
+                  contact_info: activity["events"][i]["url"],
                   price: rand(0..100))
   i += 1
-  puts "done #{i}object"
+  puts "seeded the #{i - 1} object"
+  # puts "summary: #{summary}, name #{name}, address #{address}, contact #{contact_info}, price #{price}"
 end
