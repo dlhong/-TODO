5c0ac76544926d647041f1aa6dadd9d42bd07d9e
daniel hong <dlhong197@gmail.com>
2022-09-10 16:46:31 +0200

    not showing user in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = policy_scope(User)
+    @users = policy_scope(User).reject { |user| user == current_user }
   end
 
   def show
