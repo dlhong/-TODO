f43a9999befdb4c2c61857352c089374a0b0743e
daniel hong <dlhong197@gmail.com>
2022-09-10 12:42:54 +0200

    added alert on review and saving

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -2,12 +2,22 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   updateNavbar() {
+    var lastScroll = window.scrollY;
+      console.log(lastScroll);
+
       console.log(window.scrollY);
-      console.log(window.innerHeight);
-    if (window.scrollY >= 250) {
-      this.element.classList.add("nav--hidden")
+    // if (window.scrollY >= 250) {
+    //   this.element.classList.add("nav--hidden")
+    // } else {
+    //   this.element.classList.remove("nav--hidden")
+    // }
+    if (lastScroll <= window.scrollY) {
+      console.log("We are going down");
     } else {
-      this.element.classList.remove("nav--hidden")
+      console.log("we are going up");
     }
+
+    lastScroll = window.scrollY;
+
   };
 }
